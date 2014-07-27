5a4f3d361c417ce5232e6e3244b6d281124145dd
Daniel Schmidt <dschmidt@weluse.de>
2014-07-27 16:38:43 +0200

    stuff

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 rm -R wordpress
 cd ../psv-theme/ && vagrant up && cd ../psv-wordpress
 scp -r vagrant@10.0.0.87:/var/www/wordpress .
+rm -R wordpress/wp-content/plugins/*
+rm -R wordpress/wp-content/uploads/*
 cd ../psv-theme/ && vagrant halt && cd ../psv-wordpress
