3832085880a9f159adb887320737be06c3fa7420
Daniel Schmidt <dschmidt@weluse.de>
2014-07-27 16:47:42 +0200

    stuff

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 rm -R wordpress
 cd ../psv-theme/ && vagrant up && cd ../psv-wordpress
-scp -r vagrant@10.0.0.87:/var/www/wordpress .
-rm -R wordpress/wp-content/plugins/*
-rm -R wordpress/wp-content/uploads/*
+scp -r vagrant@10.0.0.87:/var/www/wordpress . && rm -R wordpress/wp-content/uploads/* &&  rm -R wordpress/wp-content/plugins/*
 cd ../psv-theme/ && vagrant halt && cd ../psv-wordpress
---
+++ wordpress/wp-content/themes/twentyfourteen/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after.
+ *
+ * @package WordPress
+ * @subpackage STTF
+ * @since STTF 1.0
+ */
+?>
+	</div><!-- #main -->
+	<div id="separator" class="clearfix"> </div>
+
+</div><!-- #wrapper -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
