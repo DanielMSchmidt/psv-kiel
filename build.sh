ee89c3e6268af66b2441785c73144b615d9cddfd
Daniel Schmidt <dschmidt@weluse.de>
2014-07-27 17:23:36 +0200

    yeah, well, ok

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm -R wordpress
 cd ../psv-theme/ && vagrant up && cd ../psv-wordpress
-scp -r vagrant@10.0.0.87:/var/www/wordpress . && rm -R wordpress/wp-content/uploads/* &&  rm -R wordpress/wp-content/plugins/*
+scp -r vagrant@10.0.0.87:/var/www/wordpress . && rm -R wordpress/wp-content/uploads/*
+mv -f wordpress/* .
+rm -R wordpress
 cd ../psv-theme/ && vagrant halt && cd ../psv-wordpress
