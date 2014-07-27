497c1547443240d48cfa715656ade1472f03ce36
Daniel Schmidt <dschmidt@weluse.de>
2014-07-27 17:24:28 +0200

    do it by hand than

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #PSV-Wordpress
 
 1. Run ```./build.sh```
+2. Move stuff from /wordpress to this folder
 2. Commit changes
--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 cd ../psv-theme/ && vagrant up && cd ../psv-wordpress
 scp -r vagrant@10.0.0.87:/var/www/wordpress . && rm -R wordpress/wp-content/uploads/*
-mv -f wordpress/* .
+#mv -f wordpress/* .
 rm -R wordpress
 cd ../psv-theme/ && vagrant halt && cd ../psv-wordpress
